30d4f83d148f071318386a2479b27661d9cd06c4
Ramonprog <programador.ramon@gmail.com>
2024-06-15 12:44:00 -0300

    passando 'props' do comp. pai para o com. filho atraves do @Input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PrimeiroComponenteComponent } from './primeiro-componente/primeiro-componente.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PrimeiroComponenteComponent],
+  imports: [RouterOutlet, PrimeiroComponenteComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
